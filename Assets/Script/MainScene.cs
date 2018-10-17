0d8de785516dae1fbfccb5010643e3094d0dd8ac
abyand <abyand71@gmail.com>
2018-10-17 23:00:03 +0700

    progress 2

---
+++ Assets/Script/MainScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScene : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Material mTiger;
+	public Material mBunny;
+	public Material mSloth;
+	public Material mUnicorn;
+
+
+	void Start () {
+
+		if (TypeMotif.motif == 1) {
+			GetComponent<Renderer> ().material = mBunny;
+		}
+		else if (TypeMotif.motif == 2) {
+			GetComponent<Renderer> ().material = mSloth;
+		}
+		else if (TypeMotif.motif == 3) {
+			GetComponent<Renderer> ().material = mTiger;
+		}
+		else if (TypeMotif.motif == 4) {
+			GetComponent<Renderer> ().material = mUnicorn;
+		}
+
+
+	}
+
+	public void BackPressed(){
+		SceneManager.LoadScene ("menu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
---
+++ Assets/Script/changeMaterial.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changeMaterial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BunnyClick(){
+		TypeMotif.motif = 1;
+		SceneManager.LoadScene ("tesFP");
+	}
+
+	public void SlothClick(){
+		TypeMotif.motif = 2;
+		SceneManager.LoadScene ("tesFP");
+	}
+
+	public void TestClick(){
+		SceneManager.LoadScene ("tesFP");
+	}
+
+	public void TigerClick(){
+		TypeMotif.motif = 3;
+		SceneManager.LoadScene ("tesFP");
+	}
+
+	public void UnicornClick(){
+		TypeMotif.motif = 4;
+		SceneManager.LoadScene ("tesFP");
+	}
+
+
+
+}
